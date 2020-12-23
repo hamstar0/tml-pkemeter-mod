5b400c5b059ce3d2ae6e635fa18d6633b35c2bef
hamstar0 <webutek@gmail.com>
2020-12-23 00:10:46 -0800

    v1.2.0.1 - Fixed issue with text display scroll

--- PKEMeter/Logic/PKEMeterLogic_Gauges.cs
+++ PKEMeter/Logic/PKEMeterLogic_Gauges.cs
@@ -14,39 +14,47 @@ namespace PKEMeter.Logic {
 
 
 	partial class PKEMeterLogic : ILoadable {
-		private void InitializeDefaultGauge() {
-			if( this.CurrentGauge != null ) { return; }
+		private static (float b, float g, float y, float r) DefaultGaugeGet(
+					ref int proxCheckTimer,
+					ref float b,
+					ref float g,
+					ref float y,
+					ref float r ) {
+			/*if( Main.rand.NextFloat() < (1f / 60f) ) {
+				b = Main.rand.NextFloat();
+				g = Main.rand.NextFloat();
+				y = Main.rand.NextFloat();
+				r = Main.rand.NextFloat();
+			}*/
 
+			// Report proximity to first found boss
+			if( proxCheckTimer++ > 5 ) {
+				proxCheckTimer = 0;
+				NPC npc = Main.npc.FirstOrDefault( n =>
+					n?.active == true
+					&& ( n.boss || n.type == NPCID.EaterofWorldsHead )
+				);
+
+				if( npc != null ) {
+					r = ( npc.Center - Main.LocalPlayer.Center ).Length() / ( 160f * 16f );
+					r = 1f - r;
+					r = Math.Max( r, 0f );
+				} else {
+					r = 0f;
+				}
+			}
+
+			return (b, g, y, r);
+		}
+
+
+		private void InitializeDefaultGauge() {
 			float b = 0, g = 0, y = 0, r = 0;
 			int proxCheckTimer = 0;
 
-			this.CurrentGauge = ( _, __ ) => {
-				/*if( Main.rand.NextFloat() < (1f / 60f) ) {
-					b = Main.rand.NextFloat();
-					g = Main.rand.NextFloat();
-					y = Main.rand.NextFloat();
-					r = Main.rand.NextFloat();
-				}*/
-
-				// Report proximity to first found boss
-				if( proxCheckTimer++ > 5 ) {
-					proxCheckTimer = 0;
-					NPC npc = Main.npc.FirstOrDefault( n =>
-						n?.active == true
-						&& (n.boss || n.type == NPCID.EaterofWorldsHead)
-					);
-
-					if( npc != null ) {
-						r = (npc.Center - Main.LocalPlayer.Center).Length() / (160f * 16f);
-						r = 1f - r;
-						r = Math.Max( r, 0f );
-					} else {
-						r = 0f;
-					}
-				}
-
-				return (b, g, y, r);
-			};
+			if( this.CurrentGauge == null ) {
+				this.CurrentGauge = ( _, __ ) => PKEMeterLogic.DefaultGaugeGet( ref proxCheckTimer, ref b, ref g, ref y, ref r );
+			}
 		}
 
 
--- PKEMeter/Logic/PKEMeterLogic_Text.cs
+++ PKEMeter/Logic/PKEMeterLogic_Text.cs
@@ -17,85 +17,91 @@ namespace PKEMeter.Logic {
 
 
 	partial class PKEMeterLogic : ILoadable {
+		private static (string text, Color color) DefaultTextDisplay() {
+			Color color = Color.Red * ( 0.5f + ( Main.rand.NextFloat() * 0.5f ) );
+			string text = "";
+
+			NPC bossNpc = Main.npc.FirstOrDefault(
+				n => n?.active == true
+				&& ( n.boss || n.type == NPCID.EaterofWorldsHead )
+			);
+
+			switch( bossNpc?.type ?? -1 ) {
+			case -1:
+				break;
+			case NPCID.EyeofCthulhu:
+				text = "WARNING - CLASS V FREEROAM CORPOREAL FLOATER";
+				break;
+			case NPCID.KingSlime:
+				text = "WARNING - CLASS V COMPOSITE ANIMATE SEMISOLID";
+				break;
+			case NPCID.EaterofWorldsHead:
+				text = "WARNING - CLASS VI SEQUENCED NECROTIC ORGANIC";
+				break;
+			case NPCID.BrainofCthulhu:
+				text = "WARNING - CLASS VI TRANSDIM SWARMHOST FLOATER";
+				break;
+			case NPCID.SkeletronHead:
+				text = "WARNING - CLASS VI REACTIVE FREEROAM CORPOREAL POSSESSOR";
+				break;
+			case NPCID.QueenBee:
+				text = "WARNING - CLASS VI FREEROAM SWARMHOST ORGANIC";
+				break;
+			case NPCID.WallofFlesh:
+				text = "WARNING - CLASS VII VOLUMINOUS KINETIC ORGANIC AMALGAMATE";
+				break;
+			case NPCID.TheDestroyer:
+				text = "WARNING - CLASS VII SEQUENCED CONSTRUCT";
+				break;
+			case NPCID.Eyezor:
+			case NPCID.Retinazer:
+				text = "WARNING - CLASS VII PAIRED FREEROAM FLOATER CONSTRUCTS";
+				break;
+			case NPCID.SkeletronPrime:
+				text = "WARNING - CLASS VII FREEROAM MULTIFACET CONSTRUCT";
+				break;
+			//case NPCID.QueenSlime:
+			//	text = "WARNING - CLASS VII FREEROAM COMPOSITE ANIMATE SEMISOLID";
+			//	break;
+			case NPCID.Plantera:
+				text = "WARNING - CLASS VIII REACTIVE FLORAL CRAWLER";
+				break;
+			case NPCID.Golem:
+				text = "WARNING - CLASS VII POWERED MULTIFACET CONSTRUCT";
+				break;
+			case NPCID.DukeFishron:
+				text = "WARNING - CLASS IX FREEROAM AQUATIC TRANSDIM ORGANIC";
+				break;
+			case NPCID.CultistBoss:
+				text = "WARNING - CLASS VIII FREEROAM TRANSDIM ELEVATED MORTAL";
+				break;
+			//case NPCID.EmpressOfLight:
+			//	text = "WARNING - CLASS IX FREEROAM LUMINOUS DEITY";
+			//	break;
+			case NPCID.MoonLordCore:
+				text = "WARNING - CLASS X FULLTORSO ULTRADIM DEITY REMNANT";
+				break;
+			default:
+				text = "WARNING - UNKNOWN CLASS V+ PKE-EMITTING ENTITY";
+				break;
+			}
+
+			return (text, color);
+		}
+
+
+
+		////////////////
+
 		private void InitializeDefaultText() {
-			if( this.CurrentText != null ) { return; }
-
-			this.CurrentText = ( _, __, ____ ) => {
-				Color color = Color.Red * (0.5f + (Main.rand.NextFloat() * 0.5f));
-				string text = "";
-
-				NPC bossNpc = Main.npc.FirstOrDefault(
-					n=>n?.active == true
-					&& (n.boss || n.type == NPCID.EaterofWorldsHead)
-				);
-
-				switch( bossNpc?.type ?? -1 ) {
-				case -1:
-					break;
-				case NPCID.EyeofCthulhu:
-					text = "WARNING - CLASS V FREEROAM CORPOREAL FLOATER";
-					break;
-				case NPCID.KingSlime:
-					text = "WARNING - CLASS V COMPOSITE ANIMATE SEMISOLID";
-					break;
-				case NPCID.EaterofWorldsHead:
-					text = "WARNING - CLASS VI SEQUENCED NECROTIC ORGANIC";
-					break;
-				case NPCID.BrainofCthulhu:
-					text = "WARNING - CLASS VI TRANSDIM SWARMHOST FLOATER";
-					break;
-				case NPCID.SkeletronHead:
-					text = "WARNING - CLASS VI REACTIVE FREEROAM CORPOREAL POSSESSOR";
-					break;
-				case NPCID.QueenBee:
-					text = "WARNING - CLASS VI FREEROAM SWARMHOST ORGANIC";
-					break;
-				case NPCID.WallofFlesh:
-					text = "WARNING - CLASS VII VOLUMINOUS KINETIC ORGANIC AMALGAMATE";
-					break;
-				case NPCID.TheDestroyer:
-					text = "WARNING - CLASS VII SEQUENCED CONSTRUCT";
-					break;
-				case NPCID.Eyezor:
-				case NPCID.Retinazer:
-					text = "WARNING - CLASS VII PAIRED FREEROAM FLOATER CONSTRUCTS";
-					break;
-				case NPCID.SkeletronPrime:
-					text = "WARNING - CLASS VII FREEROAM MULTIFACET CONSTRUCT";
-					break;
-				//case NPCID.QueenSlime:
-				//	text = "WARNING - CLASS VII FREEROAM COMPOSITE ANIMATE SEMISOLID";
-				//	break;
-				case NPCID.Plantera:
-					text = "WARNING - CLASS VIII REACTIVE FLORAL CRAWLER";
-					break;
-				case NPCID.Golem:
-					text = "WARNING - CLASS VII POWERED MULTIFACET CONSTRUCT";
-					break;
-				case NPCID.DukeFishron:
-					text = "WARNING - CLASS IX FREEROAM AQUATIC TRANSDIM ORGANIC";
-					break;
-				case NPCID.CultistBoss:
-					text = "WARNING - CLASS VIII FREEROAM TRANSDIM ELEVATED MORTAL";
-					break;
-				//case NPCID.EmpressOfLight:
-				//	text = "WARNING - CLASS IX FREEROAM LUMINOUS DEITY";
-				//	break;
-				case NPCID.MoonLordCore:
-					text = "WARNING - CLASS X FULLTORSO ULTRADIM DEITY REMNANT";
-					break;
-				default:
-					text = "WARNING - UNKNOWN CLASS V+ PKE-EMITTING ENTITY";
-					break;
-				}
-
-				return (text, color);
-			};
-
 			Timers.SetTimer( 4, true, () => {
 				this.TextScrollPos += 2;
 				return true;
 			} );
+
+			if( this.CurrentText == null ) {
+				this.CurrentText = (_, __, ___) => PKEMeterLogic.DefaultTextDisplay();
+			}
 		}
 
 
