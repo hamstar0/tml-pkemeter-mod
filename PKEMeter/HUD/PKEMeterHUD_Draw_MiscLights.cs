3d872b74bdd4cdc711eb589c0d7a77b6f5ba6f89
hamstar0 <webutek@gmail.com>
2021-11-18 01:53:48 -0800

    v1.7.0 - Added misc light glows

--- PKEMeter/HUD/PKEMeterHUD_Draw_MiscLights.cs
+++ PKEMeter/HUD/PKEMeterHUD_Draw_MiscLights.cs
@@ -19,21 +19,31 @@ namespace PKEMeter.HUD {
 					Color c7,
 					Color c8,
 					Color c9 ) {
-			this.DrawHUDMiscLight( sb, pos, 0, c1 );
-			this.DrawHUDMiscLight( sb, pos, 4, c2 );
-			this.DrawHUDMiscLight( sb, pos, 8, c3 );
-			this.DrawHUDMiscLight( sb, pos, 12, c4 );
-			this.DrawHUDMiscLight( sb, pos, 16, c5 );
-			this.DrawHUDMiscLight( sb, pos, 20, c6 );
-			this.DrawHUDMiscLight( sb, pos, 24, c7 );
-			this.DrawHUDMiscLight( sb, pos, 28, c8 );
-			this.DrawHUDMiscLight( sb, pos, 30, c9 );
+			this.DrawHUDMiscLight1( sb, pos, 0, c1 );
+			this.DrawHUDMiscLight1( sb, pos, 4, c2 );
+			this.DrawHUDMiscLight1( sb, pos, 8, c3 );
+			this.DrawHUDMiscLight1( sb, pos, 12, c4 );
+			this.DrawHUDMiscLight1( sb, pos, 16, c5 );
+			this.DrawHUDMiscLight1( sb, pos, 20, c6 );
+			this.DrawHUDMiscLight1( sb, pos, 24, c7 );
+			this.DrawHUDMiscLight1( sb, pos, 28, c8 );
+			this.DrawHUDMiscLight1( sb, pos, 30, c9 );
+
+			this.DrawHUDMiscLight2( sb, pos, 0, c1 );
+			this.DrawHUDMiscLight2( sb, pos, 4, c2 );
+			this.DrawHUDMiscLight2( sb, pos, 8, c3 );
+			this.DrawHUDMiscLight2( sb, pos, 12, c4 );
+			this.DrawHUDMiscLight2( sb, pos, 16, c5 );
+			this.DrawHUDMiscLight2( sb, pos, 20, c6 );
+			this.DrawHUDMiscLight2( sb, pos, 24, c7 );
+			this.DrawHUDMiscLight2( sb, pos, 28, c8 );
+			this.DrawHUDMiscLight2( sb, pos, 30, c9 );
 		}
 
 
 		////////////////
 
-		private void DrawHUDMiscLight( SpriteBatch sb, Vector2 pos, int offset, Color color ) {
+		private void DrawHUDMiscLight1( SpriteBatch sb, Vector2 pos, int offset, Color color ) {
 			var destRect = new Rectangle(
 				(int)pos.X + 22 + offset,
 				(int)pos.Y + 70,
@@ -46,6 +56,22 @@ namespace PKEMeter.HUD {
 				destinationRectangle: destRect,
 				sourceRectangle: null,
 				color: color
+			);
+		}
+
+		private void DrawHUDMiscLight2( SpriteBatch sb, Vector2 pos, int offset, Color color ) {
+			var destRect = new Rectangle(
+				(int)pos.X + 20 + offset,
+				(int)pos.Y + 68,
+				6,
+				6
+			);
+
+			sb.Draw(
+				texture: Main.magicPixel,
+				destinationRectangle: destRect,
+				sourceRectangle: null,
+				color: color * 0.25f
 			);
 		}
 	}
