8f0f615ffa040d69d1f78fa61a67813956dcbcab
hamstar0 <webutek@gmail.com>
2022-05-13 04:38:38 -0700

    v3.0.0.2 - Updates (HUD Elems Libs)

--- PKEMeter/HUD/PKEMeterHUD.cs
+++ PKEMeter/HUD/PKEMeterHUD.cs
@@ -11,22 +11,27 @@ using PKEMeter.Items;
 namespace PKEMeter.HUD {
 	public partial class PKEMeterHUD : HUDElement {
 		public static PKEMeterHUD CreateDefault() {
-			Vector2 pos = PKEMeterHUD.GetBaseHUDPosition();
+			(Vector2 posOffset, Vector2 posPerc) = PKEMeterHUD.GetBaseHUDPosition();
 			Texture2D bodyTex = PKEMeterMod.Instance.GetTexture( "HUD/MeterBody" );
 
-			return new PKEMeterHUD( "PKEMeter", pos, new Vector2(bodyTex.Width, bodyTex.Height) );
+			return new PKEMeterHUD( "PKEMeter", posOffset, posPerc, new Vector2(bodyTex.Width, bodyTex.Height) );
 		}
 
 
 		////////////////
 
-		public static Vector2 GetBaseHUDPosition() {
+		public static (Vector2 posOffset, Vector2 posPerc) GetBaseHUDPosition() {
 			var config = PKEMeterConfig.Instance;
-			int posX = config.Get<int>( nameof( config.PKEMeterHUDBasePositionX ) );
-			int posY = config.Get<int>( nameof( config.PKEMeterHUDBasePositionY ) );
-			var pos = new Vector2( posX, posY );
+			int posX = config.Get<int>( nameof(config.PKEMeterHUDBasePositionX) );
+			int posY = config.Get<int>( nameof(config.PKEMeterHUDBasePositionY) );
 
-			return pos;
+			var posOffset = new Vector2( posX, posY );
+			var posPerc = new Vector2(
+				posOffset.X < 0 ? 1f : 0f,
+				posOffset.Y < 0 ? 1f : 0f
+			);
+
+			return (posOffset, posPerc);
 		}
 
 
@@ -53,8 +58,8 @@ namespace PKEMeter.HUD {
 
 		////////////////
 		
-		private PKEMeterHUD( string name, Vector2 position, Vector2 dim ) 
-					: base( name, position, dim, () => PKEMeterHUD.CanDrawPKE() ) {
+		private PKEMeterHUD( string name, Vector2 positionOffset, Vector2 positionPercent, Vector2 dim ) 
+					: base( name, positionOffset, positionPercent, dim, () => PKEMeterHUD.CanDrawPKE() ) {
 			this.MinFont = PKEMeterMod.Instance.GetTexture( "HUD/MinFont" );
 			this.MeterBody = PKEMeterMod.Instance.GetTexture( "HUD/MeterBody" );
 			this.MeterDisplay = PKEMeterMod.Instance.GetTexture( "HUD/MeterDisplay" );
